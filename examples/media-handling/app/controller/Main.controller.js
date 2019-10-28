aa93d52b4868a897e9ddbe190d785153826680de
Volker Buzek <volker.buzek@js-soft.com>
2019-10-28 11:18:57 +0100

    feat: file uploader + list of stored items

--- examples/media-handling/app/controller/Main.controller.js
+++ examples/media-handling/app/controller/Main.controller.js
@@ -1,11 +1,31 @@
 sap.ui.define([
-    "test/Sample/controller/BaseController"
-], Controller => {
+    "test/Sample/controller/BaseController",
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("test.Sample.controller.Main", {
         onInit() {
+        },
 
-        }
+        handleUploadPress(oEvent) {
+            this.byId("FileUploader").upload();
+        },
+
+        onUploadComplete(oEvent) {
+            const sResponse = oEvent.getParameter("response");
+            if (sResponse) {
+                let sMsg = "";
+                const m = /^\[(\d\d\d)\]:(.*)$/.exec(sResponse);
+                if (m && m[1] === "200") {
+                    sMsg = `Return Code ${m[1]} - ${m[2]} - Success`;
+                    oEvent.getSource().setValue("");
+                } else {
+                    sMsg = `Error: ${sResponse}`;
+                }
+
+                MessageToast.show(sMsg);
+            }
+        },
     });
 });
