eb56bc58c5a85a888b1118c0ee8d8b455148988d
Gregor Wolf <gregor.wolf@gmail.com>
2019-12-22 09:44:40 +0100

    Test media handling

--- examples/media-handling/srv/media-service.js
+++ examples/media-handling/srv/media-service.js
@@ -5,25 +5,31 @@ module.exports = srv => {
   srv.on('UPDATE', 'Pictures', async req => {
     console.log("Udate");
 
-    let content;
+    if (req.data.content) {
+      const contentType = req._.req.headers['content-type']
+      // Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryBDeMle41o08M3pD0
+      let content;
 
-    // https://areknawo.com/node-js-file-streams-explained/
-
-    // 
-    req.data.content.on("data", dataChunk => {
-      console.log(dataChunk);
-      content += dataChunk;
-      // content += <someTransformation>(dataChunk)
-    });
-
-    req.data.content.on("finish", () => {
-      console.log("Stream finish");
-      console.log(content.toString('base64'));
-    });
-
-  })
-  srv.on('READ', 'Pictures', req => {
-    console.log("Read");
+      // https://areknawo.com/node-js-file-streams-explained/
+  
+      // Read stream
+      req.data.content.on("data", dataChunk => {
+        console.log(dataChunk);
+        content += dataChunk;
+        // content += <someTransformation>(dataChunk)
+      });
+  
+      // Output stream
+      req.data.content.on("end", () => {
+        console.log("Stream ended");
+        console.log(content);
+      });  
+    } else {
+      return next()
+    }
   })
   */
+ srv.on('READ', 'Pictures', req => {
+    console.log("Read");
+  })
 }
