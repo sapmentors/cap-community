6ffb42b601cb9cdec636f4e624d2b8ff922d589b
Gregor Wolf <gregor.wolf@gmail.com>
2019-12-22 08:23:09 +0100

    Add onUploadAborted

--- examples/media-handling/appv2/controller/Main.controller.js
+++ examples/media-handling/appv2/controller/Main.controller.js
@@ -44,6 +44,14 @@ sap.ui.define([
             }
         },
 
+        onUploadAborted(oEvent) {
+            const sResponse = oEvent.getParameter("response");
+            if (sResponse) {
+                var oResponse = JSON.parse(sResponse);
+                MessageToast.show(oResponse.error.message);
+            }
+        },
+
         uuidv4() {
             return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
                 var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
